670504d57461d3611e97704a7e1f7959750a45a1
Pancham Mehta <pancham1305@gmail.com>
2023-04-07 23:36:37 +0530

    tt

--- temp.js
+++ temp.js
@@ -18,6 +18,60 @@ edf2192208 -solution.js
 74180c5872 -style.css
 f90740a936 -style.css
 e7bcc03b0f -style.css
-4fd34105fc -
-1eefc4f3fe
 */
+
+//index.js
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const btn = document.getElementById("sub");
+  const form = document.getElementById("hide");
+  const text = document.getElementById("text");
+  const img = document.getElementById("img");
+  const head = document.getElementById("head");
+  const body = document.getElementById("body");
+  const title = document.getElementById("title");
+  const getimg = document.getElementById("getimg");
+
+  await addNonce([btn, form, text, img, head, body, title, getimg]);
+});
+
+async function fixNonce(nonce) {
+  /* use `await fixNonce(nonce)` to run this function in ur DevTool Console
+   how does a devtool look like? here is an img -> */
+  return await fetch("/nonce", {
+    method: "POST",
+    body: JSON.stringify({
+      nonce,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((d) => d.json());
+}
+
+async function addNonce(elements) {
+  const { nonces } = await fetch("/nonce", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((d) => d.json());
+
+  elements.forEach((x, i) => {
+    x.setAttribute("nonce", nonces[i]);
+  });
+}
+
+// solution.js
+
+function showTruth(data) {
+  const vid = document.getElementById("vid");
+  const video = document.getElementById("video");
+  vid.style.display = "block";
+  /* SOMETHING IS HERE -> edf2192208 <- HERE IS SOMETHING */
+  if (!data) {
+    video.style.display = "flex";
+    video.src = ["solution.mp4", "solution2.mp4"][Math.round(Math.random())];
+    video.play();
+  }
+}
